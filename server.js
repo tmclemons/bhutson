f4d13bdbbd11e48e8b01fe832674aa7c0af6b1b1
Tyrone Clemons <tmclemons87@gmail.com>
2016-03-15 12:15:46 -0400

    harp serve

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var app = express();
-
-app.set('port', process.env.PORT || 1337);
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname, 'build')));
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port' + app.get('port'));
-});
+require('harp').server(__dirname, { port: process.env.PORT || 5000 })
